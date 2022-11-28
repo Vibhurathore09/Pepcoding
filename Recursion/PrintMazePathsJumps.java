ab8c849911c0e97a185acc63536aba052a17ea33
Vibhurathore09 <vibhurathoreshic@gmail.com>
2022-11-28 19:09:11 +0530

    Update

--- Recursion/PrintMazePathsJumps.java
+++ Recursion/PrintMazePathsJumps.java
@@ -7,6 +7,23 @@ public class PrintMazePathsJumps {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        
+        Print(n , m , 1, 1,"");
+        sc.close();
+    }
+    public static void Print(int sr , int sc , int dr , int dc , String ans){
+        if(sr == dr && sc == dc){
+            System.out.println(ans);
+            return;
+        }
+        for(int ms = 1 ; ms <= dc-sc ; ms++){
+            Print(sr, sc+ms, dr, dc, ans+"h"+ms);
+        }
+
+        for(int ms = 1 ; ms <= dr-sr ; ms++){
+            Print(sr+ms, sc, dr, dc, ans+"V"+ms);
+        }
+        for(int ms = 1 ; ms <= dr-sr && ms<=dc-sc ; ms++){
+            Print(sr+ms, sc+ms, dr, dc, ans+"D"+ms);
+        }
     }
 }
