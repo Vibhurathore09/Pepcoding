1a374e6ac7add77298a7bec830cb2cb08544ff93
Vibhurathore09 <vibhurathoreshic@gmail.com>
2022-11-28 18:50:26 +0530

    commit

--- Recursion/PrintStairPaths.java
+++ Recursion/PrintStairPaths.java
@@ -1,5 +1,24 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class PrintStairPaths {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PrintPaths(n , "");
+        sc.close();
+    }
+    public static void PrintPaths(int n , String s){
+        if(n == 0){
+            System.out.println(s+" ");
+            return ;
+        }
+        if(n < 0){
+            return;
+        }
+        PrintPaths(n-1, s+"1");
+        PrintPaths(n-2, s+"2");
+        PrintPaths(n-3, s+"3");
+    }
 }
